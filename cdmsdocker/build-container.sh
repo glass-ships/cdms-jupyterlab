4913d8c6c11942111436ca0f9605fab4782e6d5a
josh <glass.ships@outlook.com>
2018-10-28 22:05:41 -0600

    update dockerfile to pull from slac-jupyterhub

--- cdmsdocker/build-container.sh
+++ cdmsdocker/build-container.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 docker build \
     --build-arg SSHKEYPVT="$(cat ~/.ssh/id_rsa)" \
     --build-arg SSHKEYPUB="$(cat ~/.ssh/id_rsa.pub)" \
     --build-arg SSHHOSTS="$(cat ~/.ssh/known_hosts)" \
-    -t cdms \
+    -t cdms-jupyterlab \
     --rm \
     -f Dockerfile .
