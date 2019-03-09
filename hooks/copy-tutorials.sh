7685c5aa7b6bda354e56154959ecc4597b8a3639
glass-ships <glass.ships@outlook.com>
2019-03-08 21:09:39 -0700

    Update copy-tutorials.sh

--- hooks/copy-tutorials.sh
+++ hooks/copy-tutorials.sh
@@ -9,3 +9,11 @@ fi
 mkdir $HOME/notebooks/Tutorials/pyCAP && cp -r /packages/pyCAP/examples/* $HOME/notebooks/Tutorials/pyCAP
 mkdir $HOME/notebooks/Tutorials/scdmsPyTools && cp -r /packages/scdmsPyTools/demo/* $HOME/notebooks/Tutorials/scdmsPyTools
 mkdir $HOME/notebooks/Tutorials/Analysis && cp -r /packages/tutorials/* $HOME/notebooks/Tutorials/Analysis
+mkdir $HOME/notebooks/Tutorials/Introduction  && \
+mv -f $HOME/notebooks/Tutorials/Analysis/JupyterDemo-Jan01.ipynb $HOME/notebooks/Tutorials/Introduction/Intro to JupyterLab
+
+#pending tutorials repo structure change
+#mkdir $HOME/notebooks/Tutorials/Basic && cp -r /packages/tutorials/Basic $HOME/notebooks/Tutorials/Basic
+#mkdir $HOME/notebooks/Tutorials/Analysis && cp -r /packages/tutorials/Analysis $HOME/notebooks/Tutorials/Analysis
+#mkdir $HOME/notebooks/Tutorials/pyCAP && cp -r /packages/pyCAP/examples/* $HOME/notebooks/Tutorials/pyCAP
+#mkdir $HOME/notebooks/Tutorials/scdmsPyTools && cp -r /packages/scdmsPyTools/demo/* $HOME/notebooks/Tutorials/scdmsPyTools
