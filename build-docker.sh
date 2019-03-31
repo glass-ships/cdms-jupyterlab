57fef07bb2dc8fef48d997bed60c6705a5f98a2b
glass-ships <glass.ships@outlook.com>
2019-03-31 16:35:27 -0600

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,10 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+## Clone CDMS repositories locally
+
+git clone josh@nero:/data/git/Analysis/scdmsPyTools.git
+cd scdmsPyTools/scdmsPyTools/BatTools
+rm -rf BatCommon
+git clone josh@nero:/data/git/Reconstruction/BatCommon.git
+cd BatCommon
+rm -rf IOLibrary
+git clone josh@nero:/data/git/DAQ/IOLibrary
+cd ../../..
+git checkout master
+git submodule update --init --recursive 
+
+cd ..
+git clone josh@nero:/data/git/Analysis/pyCAP.git
+
+git clone josh@nero:/data/git/Analysis/tutorials.git
+
+git clone josh@nero:/data/git/Analysis/python_colorschemes.git
+
+git clone josh@nero:/data/git/TF_Analysis/Northwestern/analysis_tools.git
 
 docker build \
-    --build-arg SSHKEYPVT="$(cat ~/.ssh/id_rsa)" \
-    --build-arg SSHKEYPUB="$(cat ~/.ssh/id_rsa.pub)" \
-    --build-arg SSHHOSTS="$(cat ~/.ssh/known_hosts)" \
-    --build-arg SSHCONFIG="$(cat ~/.ssh/config)" \
-    -t "$3" # detlab/cdms-jupyterlab:0.1.0 \
+    -t "$2" # detlab/cdms-jupyterlab:0.1.0 \
     --rm \
     -f Dockerfile .
